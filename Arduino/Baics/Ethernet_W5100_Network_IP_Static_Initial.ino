7ed0969e304d776889e79463e46c705a8a2519b9
AZIZI-Sajjad <77125737+AZIZI-Sajjad@users.noreply.github.com>
2022-01-15 20:59:14 +0100

    Ethernet W5100 Network IP Static Initial.ino
    Ethernet_W5100_Network_IP_Static_Initial.ino
    15-01-2022

---
+++ Arduino/Baics/Ethernet_W5100_Network_IP_Static_Initial.ino
@@ -0,0 +1,59 @@
+
+
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+IPAddress ip(192, 168, 2, 123);
+IPAddress myDns(192, 168, 2, 254);
+IPAddress gateway(192, 168, 2, 254);
+IPAddress subnet(255, 255, 255, 0);
+
+
+void setup() {
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  //Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, myDns, gateway, subnet);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start listening for clients
+  // server.begin();
+
+  Serial.print("LOCAL @IP = :");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+
+}
